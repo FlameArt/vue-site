10ce91d0b5311845ed38ac75928590fcfbfaad93
Flame <flameartme@gmail.com>
2021-09-15 20:24:03 +0300

    some improvements for state

--- template/src/store/mutations.js
+++ template/src/store/mutations.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 
 export default {
-  
+
+  /**
+   * Установить произвольный компонент состояния
+   * @param state
+   * @param variable
+   * @param value
+   */
+  set (state, [variable, value]) {
+    state[variable] = value
+  },
+
   setHome(state, data) {
-    
+
     // Загружаем любые данные на стороне сервера, вызывается из Home->asyncData
     // Vue.set(state, 'objects', SERVER_PRELOADED_DATA.objects);
-    
+
   },
-  
+
 }
